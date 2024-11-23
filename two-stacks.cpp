27a80aaa48cffdaa922598d71992b046707933c1
Vedaa Anand <work.vedaa@gmail.com>
2024-11-23 20:42:12 +0530

    fix: pop1, pop2, top1, top2

--- two-stacks.cpp
+++ two-stacks.cpp
@@ -1,76 +1,79 @@
-#include <stdlib.h>
 #include <iostream>
 using namespace std;
 
-class twoStacks
-{
+class twoStacks {
 private:
-    int *arr;
+    int* arr;
     int size;
     int top1, top2;
 public:
     // constructor
-    twoStacks(int n){
+    twoStacks(int n) {
         size = n;
         arr = new int[size];
-        int top1 = -1;
-        int top2 = size - 1;
-    };
+        top1 = -1;
+        top2 = size;
+    }
 
-    // push from 1 end
-    void push1(int x){
-        if(top1 < top2 - 1){
+    // deconstructor
+    ~twoStacks() {
+        delete[] arr;
+    }
+
+    // push from right
+    void push1(int x) {
+        if (top1 < top2 - 1) {
             top1++;
             arr[top1] = x;
         } else {
-            cout << "Stack Overflow";
+            cout << "Stack Overflow\n";
         }
     }
 
-    // push from other end
-    void push2(int x){
-        if(top1 < top2 - 1){
+    // push from left
+    void push2(int x) {
+        if (top1 < top2 - 1) {
             top2--;
             arr[top2] = x;
         } else {
-            cout << "Stack Overlow";
+            cout << "Stack Overflow\n";
         }
     }
 
-    // pop from 1 end
-    int  pop1(){
-        int n = -1;
-        if(top1 == -1){
-            cout << "Stack Underflow";
+    // pop from right
+    int pop1() {
+        if (top1 == -1) {
+            cout << "Stack Underflow\n";
+            return -1;
         } else {
-            n = arr[top1];
+            int n = arr[top1];
             top1--;
+            return n;
         }
-        return n;
     }
 
-    // pop from other end
-    int pop2(){
-        int n = -1;
-        if(top2 == size - 1){
-            cout << "Stack Underflow";
+    // pop from left
+    int pop2() {
+        if (top2 == size) {
+            cout << "Stack Underflow\n";
+            return -1;
         } else {
-            n = arr[top2];
+            int n = arr[top2];
             top2++;
+            return n;
         }
-        return n;
     }
 };
 
-int main(){
-    twoStacks ts(5); 
-    ts.push1(5); 
-    ts.push2(10); 
-    ts.push2(15); 
-    ts.push1(11); 
-    ts.push2(7); 
-    cout << "Popped element from stack1 is " << ts.pop1(); 
-    ts.push2(40); 
-    cout << "\nPopped element from stack2 is " << ts.pop2(); 
-    return 0; 
+int main() {
+    twoStacks ts(5);
+    ts.push1(5);
+    ts.push2(10);
+    ts.push2(15);
+    ts.push1(11);
+    ts.push2(7);
+    cout << "Popped element from stack1 is " << ts.pop1() << endl;
+    ts.push2(40);
+    cout << "Popped element from stack2 is " << ts.pop2() << endl;
+    return 0;
 }
